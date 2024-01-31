022c5942b08a449543feb08e49c7e9f2f49bab02
TimonAME <timon-a@outlook.at>
2024-01-31 09:35:53 +0100

    Refactored slingshot game code

--- matter.js
+++ matter.js
@@ -86,18 +86,38 @@ window.addEventListener("resize", () => handleResize(matterContainer));
 
 
 //Slingshot Game
+/*
 let platform = Matter.Bodies.rectangle(1200, 500, 300, 20, { isStatic: true });
 let stack = Matter.Composites.stack(1100, 270, 4, 4, 0, 0, function(x, y) {
     return Matter.Bodies.polygon(x, y, 8, 30); 
 });
+*/
+let radius = 30; // Adjust as needed
+let rows = 8; // Adjust as needed
+let columns = 3; // Adjust as needed
 
-let ball = Matter.Bodies.circle(300, 600,20);
+let platformX = matterContainer.clientWidth * 0.8; // Adjust as needed
+let platformY = matterContainer.clientHeight * 0.75; // Adjust based on radius and rows
+let platformWidth = columns * radius * 2; // Adjust based on radius and columns
+let platform = Matter.Bodies.rectangle(platformX, platformY, platformWidth, 20, { isStatic: true });
+
+let stackX = platformX - platformWidth / 2; // Adjust based on radius and columns
+let stackY = platformY - (rows * radius * 2); // Adjust based on radius and rows
+let stack = Matter.Composites.stack(stackX, stackY, columns, rows, 0, 0, function(x, y) {
+    return Matter.Bodies.polygon(x, y, 8, radius); 
+});
+
+
+let ballX = 450;
+let ballY = 450;
+let ballRadius = 20;
+
+let ball = Matter.Bodies.circle(ballX, ballY, ballRadius);
 let sling = Matter.Constraint.create({ 
-    pointA: { x: 300, y: 600 }, 
+    pointA: { x: ballX, y: ballY }, 
     bodyB: ball, 
     stiffness: 0.05
 });
-
 
 let firing = false;
 Matter.Events.on(mouseConstraint,'enddrag', function(e) {
@@ -105,8 +125,8 @@ Matter.Events.on(mouseConstraint,'enddrag', function(e) {
 });
 
 Matter.Events.on(engine,'afterUpdate', function() {
-  if (firing && Math.abs(ball.position.x-300) < 20 && Math.abs(ball.position.y-600) < 20) {
-      ball = Matter.Bodies.circle(300, 600, 20);
+  if (firing && Math.abs(ball.position.x-ballX) < 20 && Math.abs(ball.position.y-ballY) < 20) {
+      ball = Matter.Bodies.circle(ballX, ballY, ballRadius);
       Matter.World.add(engine.world, ball);
       sling.bodyB = ball;
       firing = false;
